f58d16d919974798d39319433a84c1121d82a160
Ali Kemal Öcalan <alikemalocalan@gmail.com>
2021-11-30 21:50:05 +0300

    Create k8s-swicher.5s.sh

---
+++ k8s-swicher.5s.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# <xbar.title>K8S Config Switcher</xbar.title>
+# <xbar.version>v1.0</xbar.version>
+# <xbar.author>Ali Kemal Öcalan</xbar.author>
+# <xbar.author.github>alikemalocalan</xbar.author.github>
+# <xbar.desc>Simple K8s config switcher for working with multi k8s cluster  </xbar.desc>
+# <xbar.dependencies></xbar.dependencies>
+# <xbar.image></xbar.image>
+
+KUBE_HOME="$HOME/.kube"
+default_file_path="$KUBE_HOME/default"
+config_file="$KUBE_HOME/config"
+
+if test -f "$default_file_path"; then
+  if [[ -n $1 ]]; then
+    cat "$KUBE_HOME/$1" >$config_file
+    echo $1 >$default_file_path
+  fi
+else
+  touch $default_file_path
+  cp $config_file "$config_file.bak"
+fi
+
+echo "K8S Switcher"
+echo "--"
+default_config=$(head -n 1 $default_file_path | tr -d '\n')
+
+echo "$default_config | color=green"
+
+inactive_configs=$(eval "ls ~/.kube | grep .k8s")
+
+for f in $inactive_configs; do
+  if [ "$f" != "$default_config" ]; then
+    echo "$f | bash='$0' param1=$f terminal=false refresh=true"
+  fi
+done
